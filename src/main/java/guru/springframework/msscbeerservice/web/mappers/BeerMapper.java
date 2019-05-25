9f8cce7fd3e613221330a5c11199be6c5b9b9e45
John Thompson <john@springframework.guru>
2019-05-25 17:32:36 -0400

    Adding Mapstruct. closes #10

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto dto);
+}
