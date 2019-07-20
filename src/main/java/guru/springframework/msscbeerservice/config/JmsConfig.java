65918c9d28ec4e4bfd7b7db233c51b8643648029
John Thompson <john@springframework.guru>
2019-07-20 17:37:08 -0400

    Configure JMS. Closes #32

---
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by jt on 2019-07-20.
+ */
+@Configuration
+public class JmsConfig {
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
