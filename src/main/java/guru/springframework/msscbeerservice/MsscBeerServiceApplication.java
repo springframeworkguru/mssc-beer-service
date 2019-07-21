dfbd53c5d0aeab1f4adccf54333759585bcabecc
John Thompson <john@springframework.guru>
2019-07-21 13:52:10 -0400

    fixing broken build - closes #36

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,9 +2,8 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
